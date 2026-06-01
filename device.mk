25a389f23185f74d1ce0b996947a89bffe1a93a2
suchit-7x <gtsuchit7x@gmail.com>
2026-06-02 02:39:12 +0530

    m55xq: Disable A/B updates

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@ DEVICE_PATH := device/samsung/m55xq
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# No A/B
+AB_OTA_UPDATER := false
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
