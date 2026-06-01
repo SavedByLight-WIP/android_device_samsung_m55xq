eab4540d8f3f6fbeb1d46c034d9d4b490bc6905b
Niyush-04 <mauryapradip04@gmail.com>
2026-06-02 00:50:24 +0530

    m55xq: Import fstab.qcom from stock

--- device.mk
+++ device.mk
@@ -7,6 +7,13 @@ DEVICE_PATH := device/samsung/m55xq
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+
 # API levels
 BOARD_SHIPPING_API_LEVEL := 31
 PRODUCT_SHIPPING_API_LEVEL := 34
