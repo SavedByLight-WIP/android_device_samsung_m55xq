7c36bc6d605aca4d30077b3a3337b0dd4821a38a
Arian <arian.kulmer@web.de>
2026-06-01 19:48:46 +0530

    m55xq: Configure SoC arch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,26 @@
 
 DEVICE_PATH := device/samsung/m55xq
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a-branchprot
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo300
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a75
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := m55xq
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := taro
+
 # Inherit the proprietary files
 include vendor/samsung/m55xq/BoardConfigVendor.mk
